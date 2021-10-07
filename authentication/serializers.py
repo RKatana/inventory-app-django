0e1bd4e16e7cfdfc2f87a8cbe1981a37a25ffe0b
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-07 16:22:52 +0300

    add create and update methods to the serializer class for user

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,3 +7,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('email', 'name', 'is_staff', 'is_superuser',)
+
+
+    def create(self, validated_data):
+        """
+        Create and return a new `User` instance, given the validated data.
+        """
+        return get_user_model().objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.email = validated_data.get('email', instance.email)
+        instance.name = validated_data.get('name', instance.name)
+        instance.is_staff = validated_data.get('is_staff', instance.is_staff)
+        instance.is_superuser = validated_data.get('is_superuser', instance.is_superuser)
+        instance.save()
+        return instance
