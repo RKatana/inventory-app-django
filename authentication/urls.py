f766458a252234a04886daf22dc7a7756a319f76
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-13 20:06:06 +0300

    update urls for authentication app and project

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,18 +1,12 @@
-from django.contrib.auth import get_user_model
 from django.urls import path
-from rest_framework.generics import ListCreateAPIView
-from .views import MyObtainTokenPairView
-from rest_framework_simplejwt.views import TokenRefreshView
-from .serializers import UserSerializer
-from rest_framework.schemas import get_schema_view
-from rest_framework_swagger.views import get_swagger_view
+from rest_framework_simplejwt import views as jwt_views
+from .views import AuthUserRegistrationView,AuthUserLoginView,UserListView
 
-schema_view = get_swagger_view(title='Registration')
 
 urlpatterns = [
-    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh', TokenRefreshView.as_view(), name='token_refresher'),
-    path('users/', ListCreateAPIView.as_view(queryset=get_user_model().objects.all(), serializer_class=UserSerializer)),
-    
-
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', AuthUserRegistrationView.as_view(), name='register'),
+    path('login', AuthUserLoginView.as_view(), name='login'),
+    path('users', UserListView.as_view(), name='users'),
 ]
