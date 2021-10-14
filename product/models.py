8c118c7f0a51d166cdf2c79b043d82851b0957e1
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-14 08:35:36 +0300

    update product model properties

--- product/models.py
+++ product/models.py
@@ -3,14 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-  product = models.ProductField(upload_to='products')
   product_name =  models.CharField(max_length=200)
-  product_desc = models.TextField()
-  product_quantity = models.CharField()
+  product_desc = models.TextField(blank=True, null=True)
+  product_quantity = models.IntegerField(max_length=250)
   product_bp = models.IntegerField(blank=True, null=True)
   product_sp = models.IntegerField(blank=True, null=True)
   product_payment = models.IntegerField(blank=True, null=True)
-  store_id = models.ForeignKey(to, on_delete)
   date_upload = models.DateTimeField(auto_now_add=True)
 
   def save_product(self):
@@ -32,5 +30,5 @@ class Product(models.Model):
     except Product.DoesNotExist:
       print('Product does not exist')
       
-		def __str__(self):
-			return self.product_name
+  def __str__(self):
+    return self.product_name
