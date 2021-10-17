79b640f5e5025e5d95d84daf2216687d8323cd77
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-10-17 20:01:58 +0300

    updated views.py and serializers.py

--- store/serializer.py
+++ store/serializer.py
@@ -1,5 +1,76 @@
+from django.contrib.auth.models import update_last_login
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from authentication.models import User
+from django.contrib.auth import authenticate
 from .models import Store
 from rest_framework import serializers
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'email', 'password',)
+        
+    def create(self, validated_data):
+        auth_user = User.objects.create_user(**validated_data)
+        return auth_user
+    
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+    access = serializers.CharField(read_only=True)
+    refresh = serializers.CharField(read_only=True)
+    role = serializers.CharField(read_only=True)
+
+    def create(self, validated_data):
+        pass
+
+
+    def update(self, instance, validated_data):
+        pass
+
+    def validate(self, data):
+        email = data['email']
+        password = data['password']
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid login credentials")
+        
+        try:
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh)
+            access_token = str(refresh.access_token)
+
+            update_last_login(None, user)
+
+            validation = {
+                'access': access_token,
+                'refresh': refresh_token,
+                'email': user.email,
+                'role': user.role
+            }
+
+            return validation
+
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Invalid login credentials')
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'role')
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super(MyTokenObtainPairSerializer,cls).get_token(user)
+        token['email'] = user.email
+        return token
 
 
 class StoreSerializer(serializers.ModelSerializer):
--- store/views.py
+++ store/views.py
@@ -1,11 +1,66 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from authentication.models import User
+
+from authentication.serializers import UserStoreListSerializer, UserRegistrationSerializer
 from .serializer import StoreSerializer
 from .models import Store
 from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 # Create your views here.
+
+class AuthUserRegistrationView(APIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        valid = serializer.is_valid(raise_exception=True)
+
+        if valid:
+            serializer.save()
+            status_code = status.HTTP_201_CREATED
+
+            response = {
+                'success': True,
+                'statusCode': status_code,
+                'message': 'User successfully registered!',
+                'user': serializer.data
+            }
+
+            return Response(response, status=status_code)
+
+class StoreListView(APIView):
+    serializer_class = UserStoreListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        if user.role != 2:
+            response = {
+                'success': False,
+                'status_code': status.HTTP_403_FORBIDDEN,
+                'message': 'You are not authorized to perform this action'
+            }
+            return Response(response, status.HTTP_403_FORBIDDEN)
+        else:
+            users = User.objects.all()
+            serializer = self.serializer_class(users, many=True)
+            response = {
+                'success': True,
+                'status_code': status.HTTP_200_OK,
+                'message': 'Successfully fetched users',
+                'users': serializer.data
+
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
 
 class StoreViewSet(ReadOnlyModelViewSet):
         
