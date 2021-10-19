ee05aadc9c30b36a19f042c7ad1a828365fb7308
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-10-19 11:25:33 +0300

    Add field user to store

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.conf import settings
 from django.db import models
+from django.db.models.deletion import CASCADE
+
 
 
 
@@ -11,7 +13,7 @@ class Store(models.Model):
 	name = models.CharField(max_length=100)
 	location = models.CharField(max_length=100)
 	uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
-
+	user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=CASCADE,null=True,blank=True)
 	
     
 	def __str__(self):
