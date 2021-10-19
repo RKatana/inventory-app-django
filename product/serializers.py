4c09ccadaadb8c7e8bb691591557519d465b6c5e
Muturi403 <faith.muturi@student.moringaschool.com>
2021-10-19 11:26:58 +0300

    replace fields with all in serializer file

--- product/serializers.py
+++ product/serializers.py
@@ -4,4 +4,4 @@ from product.models import Product
 class ProductSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Product
-		fields = ['name', 'quantity', 'buying_price', 'selling_price', 'spoilt', 'payment_status', 'uid', 'store']
+		fields = '__all__'
