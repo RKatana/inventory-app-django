4f5cb73b482694c4e528ca13da4dda489f38b7c9
Muturi403 <faith.muturi@student.moringaschool.com>
2021-10-20 13:43:51 +0300

    add delete product parameter in the view file

--- product/views.py
+++ product/views.py
@@ -28,6 +28,21 @@ class  CreateProductView(APIView):
             }
             return Response(response, status=status_code)
         
+    @swagger_auto_schema(request_body=ProductSerializer, responses={201: ProductSerializer(many=True)})
+    def delete(self, request):
+        serializer = self.serializer_class(data=request.data)
+        valid = serializer.is_valid(raise_exception=True)
+        if valid:
+            serializer.save()
+            status_code = status.HTTP_201_CREATED
+            response = {
+                'success': True,
+                'statusCode': status_code,
+                'message': 'Product successfully deleted',
+                'product': serializer.data
+            }
+            return Response(response, status=status_code)
+        
 class ProductListView(APIView):    
     serializer_class = ProductSerializer
     permission_classes = (AllowAny,)
@@ -40,6 +55,18 @@ class ProductListView(APIView):
             'success': True,
             'status_code': status.HTTP_200_OK,
             'message': 'Successfully fetched products',
+            'products': serializer.data,
+        }
+        return Response(response, status=status.HTTP_200_OK)
+    
+    @swagger_auto_schema(manual_parameters=[], responses={200: ProductSerializer(many=True)})
+    def delete(self, request):
+        products = Product.objects.all()
+        serializer = self.serializer_class(products, many=True)
+        response = {
+            'success': True,
+            'status_code': status.HTTP_200_OK,
+            'message': 'Successfully deleted products',
             'products': serializer.data,
         }
         return Response(response, status=status.HTTP_200_OK)
@@ -58,6 +85,18 @@ class ProductByIdView(APIView):
             'success': True,
             'status_code': status.HTTP_200_OK,
             'message': 'Successfully fetched product',
+            'product': serializer.data,
+        }
+        return Response(response, status=status.HTTP_200_OK)
+    
+    @swagger_auto_schema(manual_parameters=[], responses={200: ProductSerializer(many=True)})
+    def delete(self, request, uid):
+        product = Product.objects.get(id=uid)
+        serializer = self.serializer_class(product, many=True)
+        response = {
+            'success': True,
+            'status_code': status.HTTP_200_OK,
+            'message': 'Successfully deleted product',
             'product': serializer.data,
         }
         return Response(response, status=status.HTTP_200_OK)
