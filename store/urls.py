7f5f9389af3f59bc018f34927945072ac39d158b
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-21 22:24:33 +0300

    update url details for store app

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,6 @@ from .views import StoreByUserIdView, StoreDetail, StoreListView
 
 urlpatterns = [
     path('stores/', StoreListView.as_view(), name='stores'),
-    # path('singlestore/<pk>',StoreByIdView.as_view(), name='singlestore'),
-    # path('createstore/', CreateStoreView.as_view(), name='createstore'),
     path('storebyuserid/<pk>',StoreByUserIdView.as_view(), name='storebyuserid'),
     path('storedetail/<int:pk>/', StoreDetail.as_view(), name='store_details'),
 ]
