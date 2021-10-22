3aac07452d2031a310a82e53afa62010cfced1fb
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-22 15:39:27 +0300

    update endpoints for retrieving storebyuserid and productbystoreid

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductDetail, ProductListView, ProductCreateView
+from .views import ProductDetail, ProductListView, ProductCreateView, ProductByStoreIdView
 
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product_list'),
     path('productdetail/<int:pk>/', ProductDetail.as_view(), name='product_details'),
-    path('createproduct/', ProductCreateView.as_view(), name='create_product')
+    path('createproduct/', ProductCreateView.as_view(), name='create_product'),
+    path('productbystoreid/<int:pk>',ProductByStoreIdView.as_view(), name='product_by_store_id'),
 ]
